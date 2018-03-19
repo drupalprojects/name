778b97c67128e46dc64de71f2fb70572d8b36650
aland <aland@198838.no-reply.drupal.org>
2018-03-19 18:01:13 +1000

    Issue #2954128 by Alan D.: Base token tests (third party)

--- src/Tests/NameTestTrait.php
+++ src/Tests/NameTestTrait.php
@@ -19,25 +19,31 @@ trait NameTestTrait {
    *   The entity type.
    * @param string $bundle
    *   The bundle.
+   * @param array $storage_extras
+   *   Additional field storage settings.
+   *   - cardinality (int): -1
+   *   - settings (array):
+   * @param array $field_extras
+   *   Additional field settings.
+   *   - widget: ['type' => 'options_buttons']
+   *   - settings (array):
    *
    * @return \Drupal\Core\Field\FieldDefinitionInterface
    *   The field definition.
    */
-  public function createNameField($field_name, $entity_type, $bundle) {
+  public function createNameField($field_name, $entity_type, $bundle, array $storage_extras = [], array $field_extras = []) {
     FieldStorageConfig::create([
       'field_name' => $field_name,
       'entity_type' => $entity_type,
       'type' => 'name',
-    ])
-      ->save();
+    ] + $storage_extras)->save();
 
     $field_config = FieldConfig::create([
       'field_name' => $field_name,
       'entity_type' => $entity_type,
       'type' => 'name',
       'bundle' => $bundle,
-    ]);
-
+    ] + $field_extras);
     $field_config->save();
     return $field_config;
   }
